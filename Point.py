566c6195918ed3a6f37ed549ba36ba19c0428012
qthequartermasterman <qthequartermasterman@gmail.com>
2021-04-27 11:48:02 -0500

    Began implementing sympy. Fixed Equality and Hash of points

--- Point.py
+++ Point.py
@@ -1,24 +1,26 @@
 import math
 import matplotlib.pyplot as plt
-from decimal import Decimal
+# from decimal import Decimal
 from Object import Object
 import numpy as np
+import sympy
 
 
 class Point(Object):
-    def __init__(self, x: float, y: float, name: str = ''):
+    def __init__(self, x: sympy.core.expr.Expr, y: sympy.core.expr.Expr, name: str = ''):
         super().__init__()
-        self.x = Decimal(x)
-        self.y = Decimal(y)
+        self.x = sympy.core.sympify(x)
+        self.y = sympy.core.sympify(y)
         self.name = name
-        self.threshold = Decimal('1.0')**8
+        #self.threshold = Decimal('1.0')**8
 
     def __eq__(self, other):
         #return self.x == other.x and self.y == other.y
         #return math.isclose(self.x, other.x) and math.isclose(self.y, other.y)
-        threshold = self.threshold
+        #threshold = self.threshold
         if isinstance(other, Point):
-            return self.x.quantize(threshold) == other.x.quantize(threshold) and self.y.quantize(threshold) == other.y.quantize(threshold)
+            #return self.x.quantize(threshold) == other.x.quantize(threshold) and self.y.quantize(threshold) == other.y.quantize(threshold)
+            return self.x.equals(other.x) and self.y.equals(other.y)
         else:
             return False
 
@@ -44,12 +46,13 @@ class Point(Object):
         return Decimal.sqrt(self * self)
 
     def __repr__(self):
-        return f'Point {self.name}: ({self.x:.4f}, {self.y:.4f})'
+        #return f'Point {self.name}: ({self.x:.4f}, {self.y:.4f})'
+        return f'Point {self.name}: ({self.x}, {self.y})'
 
     def __hash__(self):
         #return hash(repr(self))
-        threshold = self.threshold
-        return hash((self.x.quantize(threshold), self.y.quantize(threshold)))
+        #threshold = self.threshold
+        return hash((self.x, self.y))
 
     def plt_draw(self) -> plt.Circle:
         return plt.Circle((self.x, self.y), radius=0.02)
@@ -59,3 +62,5 @@ class Point(Object):
 
     def normalize(self):
         return abs(self)*self
+
+p = Point(2,3)
