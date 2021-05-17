e44ca9d14d00d43f772ad0420e97a44f43dc3770
qthequartermasterman <qthequartermasterman@gmail.com>
2021-05-16 20:21:13 -0500

    Improved Line Unit Tests (including test_constants)

---
+++ tests/test_constants.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+
+
+class GeometryTestCase(TestCase):
+    def assertHashEqual(self, object1, object2, *args, **kwargs):
+        return self.assertEqual(hash(object1), hash(object2), *args, **kwargs)
+
+    def assertHashNotEqual(self, object1, object2, *args, **kwargs):
+        return self.assertNotEqual(hash(object1), hash(object2), *args, **kwargs)
+
+# Set of coordinates that should cover many useful test cases
+coordinates = [(0, 0), (1, 0), (0, 1), (3, 2),
+               ('1/2', 0), ('sqrt(3)', 1), ('exp(2)', '1/2'), ('-sqrt(3)', 'sqrt(1)')]
