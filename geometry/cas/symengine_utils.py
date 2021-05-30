eb4d6ee121d715edd778967546ce41167d308e6d
qthequartermasterman <qthequartermasterman@gmail.com>
2021-05-29 21:13:06 -0500

    Symengine utils now caches the result of each function for speed in repeated computation.

--- geometry/cas/symengine_utils.py
+++ geometry/cas/symengine_utils.py
@@ -1,20 +1,33 @@
 from symengine import Expr, Eq, sympify, nan
 from sympy.simplify import sqrtdenest
+from sympy import simplify
+
+from functools import lru_cache
 
 from typing import Union
 Expression = Union[Expr, str, int, float]  # Anything that is sympify-able
 
 
+@lru_cache
 def is_nan(element: Expression):
     element = sympify(element)
     return isinstance(element, type(nan))
 
+
+@lru_cache
 def symengine_equality(a: Expr, b: Expr):
     return Eq(a, b).simplify()
 
 
+@lru_cache
 def optimized_simplify(expr: Expr) -> Expr:
     #return sqrtdenest(expr)
     return expr.expand()
+    #return simplify(expr)
+    #return sqrtdenest(expr).expand()
+
+@lru_cache
+def full_simplify(expr: Expr) -> Expr:
+    return simplify(optimized_simplify(expr))
 
 
